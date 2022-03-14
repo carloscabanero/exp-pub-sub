2cf401d3d92e8da418fe934728c8a8b8aff7626e
Carlos Cabanero <carlos@blink.sh>
2022-03-14 21:18:36 +0000

    Compiling with Box, but not able to call work
    - This is because work gets captured in the loop. It doesn't happen as a
    function parameter though because we are passing the Trait instead of the Box.

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,60 @@ impl FromPubSubMessage for MachineStatsPacket {
     }
 }
 
+// https://stackoverflow.com/questions/41081240/idiomatic-callbacks-in-rust
+struct Worker<T> where
+    T: FromPubSubMessage + Send + 'static + std::fmt::Debug
+{
+// NOTE The Fn has a struct associated and we do not know the size when we send it, so it may require an
+// Arc or Box to store it.
+    subscription: Arc<Subscription>,
+    work: Box<dyn Fn(T) + Send + Sync + 'static>,
+}
+
+impl <T> Worker<T> where
+    T: FromPubSubMessage + Send + 'static + std::fmt::Debug
+{
+    // We may probably wrap the client ourselves too, so the "token" and stuff like that can be taken out.
+    // Then the worker would just receive "work".
+    fn new(subscription: Arc<Subscription>, work: impl Fn(T) + Sync + Send + 'static) -> Self
+    {
+        Worker {
+            subscription: subscription,
+            work: Box::new(work)
+        }
+    }
+    // We could refactor to not store the function and just run it (maybe). Or use scoped threads.
+    fn run(self) {
+        task::spawn(async move {
+            while self.subscription.client().is_running() {
+                match self.subscription.get_messages::<T>().await {
+                    Ok(messages) => {
+                        for (result, ack_id) in messages {
+                            match result {
+                                Ok(message) => {
+                                    //info!("recieved {:?}", message);
+                                    let subscription = Arc::clone(&self.subscription);
+                                    // T needs to be 'static just to help the compiler deal with it.
+                                    // Not an issue as it is moved.                                    
+                                    task::spawn(async move {
+                                        println!("{:?}", message);
+                                        //self.work)(message);
+                                        subscription.acknowledge_messages(vec![ack_id]).await;
+                                    });
+                                }
+                                Err(e) => error!("Failed converting to UpdatePacket: {}", e),
+                            }
+                        }
+                    }
+                    Err(e) => error!("Failed to pull PubSub messages: {}", e),
+                }
+            }
+            debug!("No longer pulling");
+        });
+
+    }
+}
+
 // We need an Arc here as the Subscription/Client needs to continuously renew the token in the background. (!!)
 // All this complexity should have been hidden as part of whatever the subscription does with the client.
 // I guess we can wrap it.
